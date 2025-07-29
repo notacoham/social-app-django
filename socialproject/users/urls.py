52412fd314a55ae9163f7e48bec61dfb6f96734b
notacoham <alexcottam12@gmail.com>
2025-07-29 01:01:06 -0600

    add password change and password change done templates

--- socialproject/users/urls.py
+++ socialproject/users/urls.py
@@ -5,5 +5,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='users/password_change_form.html'), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'), name='password_change_done')
 ]
